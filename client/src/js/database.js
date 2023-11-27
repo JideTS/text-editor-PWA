38c9c34928092d2790c37a5332be098e2c51baf9
Shaduruk <jide@tidjani-serpos.com>
2023-11-27 09:22:27 -0500

    fix bugs before deployment

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ content: content });
+  const request = store.put({ id:1, value: content});
   const result = await request;
   console.log('Data saved to the database', result);
 };
@@ -33,7 +33,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 initdb();
