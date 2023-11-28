30cef0a425898d622d776a7cbbf9c41ae1db64f4
Shaduruk <jide@tidjani-serpos.com>
2023-11-27 20:48:55 -0500

    deployment

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
