c7a810040f067648c54ba431398c1424432e6956
Shaduruk <jide@tidjani-serpos.com>
2023-11-27 22:54:58 -0500

    deploy

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,6 +1,6 @@
-const path = require('path');
+// const path = require('path');
 
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.join('./index.html'))
+  res.sendFile('index.html', { root: __dirname})
   );
